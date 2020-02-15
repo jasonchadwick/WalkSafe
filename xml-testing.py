4acccb00a2ee6fb3d52706005c3134b897df5974
jasonchadwick <jasonchadwick@users.noreply.github.com>
2020-02-14 20:03:54 -0500

    fixed testing

--- xml-testing.py
+++ xml-testing.py
@@ -1,7 +1,8 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse("manhattan-map.osm")
+tree = ET.parse("maps/manhattan-map.osm")
 root = tree.getroot()
+
 for way in root.findall("way"):
     if way.get("id") == "5668968":
         for tag in way.findall("tag"):
