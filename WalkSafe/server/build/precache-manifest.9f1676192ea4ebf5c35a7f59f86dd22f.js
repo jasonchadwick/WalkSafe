b9fff4d084d5235b449f2e849b0de6a41bb595f3
Adrian <47037839+akager@users.noreply.github.com>
2020-02-15 15:01:34 -0500

    k

---
+++ WalkSafe/server/build/precache-manifest.9f1676192ea4ebf5c35a7f59f86dd22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268d920966ed50a8c4d88dde36a191dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6d5ae6ca2270dcd8191",
+    "url": "/static/css/2.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "5bfd2c43d4e3b41e67f8",
+    "url": "/static/css/main.b6b64a65.chunk.css"
+  },
+  {
+    "revision": "b6d5ae6ca2270dcd8191",
+    "url": "/static/js/2.f21524ab.chunk.js"
+  },
+  {
+    "revision": "5bfd2c43d4e3b41e67f8",
+    "url": "/static/js/main.6d4b8ebe.chunk.js"
+  },
+  {
+    "revision": "5291d6c3c714964787bf",
+    "url": "/static/js/runtime-main.75f293c5.js"
+  }
+]);
