c877a4ce7324196d5e3817703a69a26026693795
jasonchadwick <jasonchadwick@users.noreply.github.com>
2020-02-15 11:32:05 -0500

    testing

--- calcRisk.py
+++ calcRisk.py
@@ -2,13 +2,17 @@ import math
 import numpy as np
 import util
 
+def calcRiskTest(node, crimedict):
+  timedict = {}
+  for t in range(24):
+    timedict[t] = 0
+
 def calcRisk(node, crimedict):
   nodelat = node[0]
   nodelon = node[1]
   timedict = {}
   for t in range(24):
-    timedict[t] = 1 #change later
-  return timedict
+    timedict[t] = 0
   for lat in [nodelat - 0.001, nodelat, nodelat + 0.001]:
     for lon in [nodelon - 0.001, nodelon, nodelon + 0.001]:
       rlat = round(lat,3)
@@ -19,4 +23,5 @@ def calcRisk(node, crimedict):
           dist = 5280*util.getDist((rlat, rlon), (crime[0],crime[1]))
           hour = crime[3][0]
           minute = crime[3][1]
-          timedict[t] += np.exp(-dist*dist/(125000))
+          dt = min(abs(hour - t), abs(24 - (hour - t)))
+          timedict[t] += crime[2] * np.exp(-dist*dist/125000) * np.exp(-dt*dt/18)
