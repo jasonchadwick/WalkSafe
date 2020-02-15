000bf8709bd3852c65cb89301ead941f8b53b865
Adrian <47037839+akager@users.noreply.github.com>
2020-02-15 14:45:29 -0500

    server

---
+++ WalkSafe/src/App.jsx
@@ -0,0 +1,24 @@
+﻿import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Blank from "./components/Blank";
+
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route exact path = "/" component = { Blank } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
