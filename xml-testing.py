68b43c3f74b7493fa5f5c7c0002d4d2098bfb7b6
jasonchadwick <jasonchadwick@users.noreply.github.com>
2020-02-14 20:20:53 -0500

    getting all roads

--- xml-testing.py
+++ xml-testing.py
@@ -1,10 +1,19 @@
 import xml.etree.ElementTree as ET
 
+def is_road(way):
+    for tag in way.findall("tag"):
+        if tag.get("k") == "highway":
+            return True
+    return False
+
 tree = ET.parse("maps/manhattan-map.osm")
 root = tree.getroot()
+ways = [x for x in root.findall("way") if is_road(x)]
 
-for way in root.findall("way"):
-    if way.get("id") == "5668968":
-        for tag in way.findall("tag"):
-            if tag.get("k") == "highway":
-                print(tag.get("v"))
+print(ways[0])
+
+#for way in root.findall("way"):
+ #   if way.get("id") == "5668968":
+  #      for tag in way.findall("tag"):
+   #         if tag.get("k") == "highway":
+    #            print(tag.get("v"))
