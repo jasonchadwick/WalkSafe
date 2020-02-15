272c5fe5f8738eff7eb79c0b6153164991b2e50d
jasonchadwick <jasonchadwick@users.noreply.github.com>
2020-02-15 12:19:25 -0500

    faster?

--- calcRisk.py
+++ calcRisk.py
@@ -21,11 +21,10 @@ def calcRisk(node, crimedict):
       rlat = round(lat,3)
       rlon = round(lon,3)
       crimes = crimedict[(rlat, rlon)]
-      for t in range(24):
-        for crime in crimes:
-          dist = 5280*util.getDist((rlat, rlon), (crime[0],crime[1]))
-          hour = crime[3][0]
-          minute = crime[3][1]
+      for crime in crimes:
+        dist = 5280*util.getDist((rlat, rlon), (crime[0],crime[1]))
+        hour = crime[3][0]
+        for t in [(hour-2)%24,(hour-1)%24,hour,(hour+1)%24,(hour+2)%24]:
           dt = min(abs(hour - t), abs(24 - (hour - t)))
           timedict[t] += crime[2] * np.exp(-dist*dist/125000) * np.exp(-dt*dt/18)
   return timedict
